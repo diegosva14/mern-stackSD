b8831b7c482127d5dd193bfb4ab444ca4e3c1aca
diegosva14 <diegosotovalle14@gmail.com>
2023-11-03 19:42:47 -0700

    cuarto commit

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://diego:x5N97p4UGzRwHlsf@cluster0.wnjcwri.mongodb.net/?retryWrites=true&w=majority';
+const uri = MONGO_CONNECT_URI;
 mongoose.connect(uri).then(db => console.log('db is conected')).catch(err => console.log(err));
 
 
