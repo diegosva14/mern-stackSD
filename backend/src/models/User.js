033f93d44d8c6b6245e9f64349e7f9a6cfaf8b59
diegosva14 <diegosotovalle14@gmail.com>
2023-11-14 21:00:06 -0800

    nuevo

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,12 +1,23 @@
-const { Schema, model } = require('mongoose');
-const userSchema = new Schema({
+const mongoose = require('mongoose')
+
+
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        trim: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
         unique: true
     },
+    password: {
+        type: String,
+        required: true
+    }
 }, {
     timestamps: true
-});
-module.exports = model('User', userSchema);
+})
+
+module.exports = mongoose.model('Users', userSchema)
