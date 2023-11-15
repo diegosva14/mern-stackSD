a4c27068b7b6be5cd04d7a52a18d5b9bad3deb4a
diegosva14 <diegosotovalle14@gmail.com>
2023-11-15 00:31:28 -0800

    nuevo

--- backend/src/server.js
+++ backend/src/server.js
@@ -42,7 +42,7 @@ if(process.env.NODE_ENV === 'production'){
 
 
 // Listen Server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT)
 })
