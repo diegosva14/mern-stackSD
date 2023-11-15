5ba21c46c9c212b5cc544dfd1845785b2e0439c8
diegosva14 <diegosotovalle14@gmail.com>
2023-11-14 21:29:01 -0800

    nuevo

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -1,29 +1,62 @@
-const usersCtrl = {};
-const User = require('../models/User');
-usersCtrl.getUsers = async (req, res) => {
-    const users = await User.find();
-    res.json(users);
-}
-usersCtrl.createUser = async (req, res) => {
-    console.log(req.body)
-    const { username } = req.body;
-    const newUser = new User({username});
-    console.log(newUser);
-    await newUser.save();
-    res.json({message: 'User created'});
-}
-usersCtrl.deleteUser = async (req, res) => {
-    await User.findByIdAndDelete(req.params.id);
-    res.json({message: 'User Deleted'})
-}
-usersCtrl.getUser = async (req, res) =>{
-    const user = await User.findById(req.params.id);
-    res.json(user)
-}
-usersCtrl.editUser = async (req, res) =>{
-    const {username} = req.body;
-    await User.findByIdAndUpdate({_id:req.params.id},{username});
-    res.json({message: 'updated'})
+
+const Users = require('../models/User');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const userCtrl = {
+    registerUser: async (req, res) =>{
+        try {
+            const {username, email, password} = req.body;
+            const user = await Users.findOne({email: email})
+            if(user) return res.status(400).json({msg: "The email already exists."})
+
+            const passwordHash = await bcrypt.hash(password, 10)
+            const newUser = new Users({
+                username: username,
+                email: email,
+                password: passwordHash
+            })
+            await newUser.save()
+            res.json({msg: "Sign up Success"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    loginUser: async (req, res) =>{
+        try {
+            const {email, password} = req.body;
+            const user = await Users.findOne({email: email})
+            if(!user) return res.status(400).json({msg: "User does not exist."})
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(!isMatch) return res.status(400).json({msg: "Incorrect password."})
+
+            // if login success create token
+            const payload = {id: user._id, name: user.username}
+            const token = jwt.sign(payload, process.env.TOKEN_SECRET, {expiresIn: "1d"})
+
+            res.json({token})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    verifiedToken: (req, res) =>{
+        try {
+            const token = req.header("Authorization")
+            if(!token) return res.send(false)
+
+            jwt.verify(token, process.env.TOKEN_SECRET, async (err, verified) =>{
+                if(err) return res.send(false)
+
+                const user = await Users.findById(verified.id)
+                if(!user) return res.send(false)
+
+                return res.send(true)
+            })
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    } 
 }
 
-module.exports = usersCtrl;
+module.exports = userCtrl;
