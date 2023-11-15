e1e0b078aa9830ecc586b78b90c5a35e5e0bfc1f
diegosva14 <diegosotovalle14@gmail.com>
2023-11-14 22:43:45 -0800

    nuevo

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,6 @@ app.set('port', process.env.PORT || 4000);
 app.use(cors());
 app.use(express.json());
 // routes
-app.use('/api/users', require('./routes/users'));
+app.use('/users', require('./routes/users'));
 app.use('/api/notes', require('./routes/notes'));
 module.exports = app;
