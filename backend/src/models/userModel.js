720fa09e288c07e4f231330ba7bc0604372c9367
diegosva14 <diegosotovalle14@gmail.com>
2023-11-29 10:47:44 -0800

    commit

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -19,7 +19,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    profilePicture: {
+        type: String,
+        default: '../assets/profile-pic.png'
+      }
 }, {
     timestamps: true
 })
