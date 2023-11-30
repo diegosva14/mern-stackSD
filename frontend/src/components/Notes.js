dc12afd014e741e5abd49958784249a099065f4e
diegosva14 <diegosotovalle14@gmail.com>
2023-11-30 12:11:09 -0800

    commit

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -3,19 +3,19 @@ import Header from './notes/Nav'
 import Home from './notes/Home'
 import CreateNote from './notes/CreateNote'
 import EditNote from './notes/EditNote'
-//import UserProfile from './notes/UserProfile'; 
+import UserProfile from './notes/UserProfile'; 
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 export default function Notes({setIsLogin}) {
     return (
         <Router>
         <div className="notes-page">
-            <Header setIsLogin={setIsLogin} />
+            <Header setIsLogin={setIsLogin} userInfo={userInfo}/>
             <section>
                 <Route path="/" component={Home} exact />
                 <Route path="/create" component={CreateNote} exact />
                 <Route path="/edit/:id" component={EditNote} exact />
-                
+                <Route path="/profile/:username" component={UserProfile} /> 
             </section>
             
         </div>
