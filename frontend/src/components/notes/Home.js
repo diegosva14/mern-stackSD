e6f73b9991b384f6fe9d4f443fff7b8365516461
diegosva14 <diegosotovalle14@gmail.com>
2023-11-29 17:36:39 -0800

    commit

--- frontend/src/components/notes/Home.js
+++ frontend/src/components/notes/Home.js
@@ -36,22 +36,26 @@ export default function Home() {
     }
 
     return (
-        <div className="container">
-        {notes.map(note => (
-          <div className="note-card" key={note._id}>
-            <div className="note-header">
-              <h2>{note.name} - {note.title}</h2>
-              <button className="close" onClick={() => deleteNote(note._id)}>X</button>
-            </div>
-            <p>{note.content}</p>
-            <div className="note-footer">
-              <span>{format(note.createdAt)}</span>
-              <Link to={`edit/${note._id}`} >Edit</Link>
-            </div>
-          </div>
-        ))}
-      </div>
-
        
+        <div className="note-wrapper">
+            {
+                notes.map(note =>(
+                    <div className="card" key={note._id}>
+                        <h4 title={note.title}>{note.title}</h4>
+                        <div className="text-wrapper">
+                            <p>{note.content}</p>
+                        </div>
+                        <p className="date">{format(note.createdAt)}</p>
+                        <div className="card-footer">
+                            {note.name}
+                            <Link to={`edit/${note._id}`} >Edit</Link>
+                        </div>
+                        <button className="close" 
+                        onClick={() => deleteNote(note._id)} >X</button>
+                    </div>
+                ))
+            }
+            
+        </div>
     )
 }
