a2fb7b04e0b021c6889f14ed1197c8846d3aae95
diegosva14 <diegosotovalle14@gmail.com>
2023-11-30 11:55:47 -0800

    commit

--- frontend/src/components/notes/Nav.js
+++ frontend/src/components/notes/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default function Nav({setIsLogin}) {
+export default function Nav({setIsLogin,userInfo}) {
 
     const logoutSubmit = () =>{
         localStorage.clear()
