6ce165b7cedd572e6b636da0cefee5ccff33638c
diegosva14 <diegosotovalle14@gmail.com>
2023-11-30 15:39:31 -0800

    commit

--- frontend/src/components/notes/Nav.js
+++ frontend/src/components/notes/Nav.js
@@ -16,7 +16,11 @@ export default function Nav({setIsLogin}) {
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/create">ADD POST</Link></li>
-    
+                {userInfo && (
+                <li>
+                <Link to="/my-profile">{userInfo.username}</Link>
+                </li>
+      )}
                 <li onClick={logoutSubmit}><Link to="/">Logout</Link></li>
             </ul>
         </header>  
--- frontend/src/components/notes/UserProfile.js
+++ frontend/src/components/notes/UserProfile.js
@@ -1,85 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import axios from 'axios';
+// UserProfile.js
+import React from 'react';
 
-const UserProfile = () => {
-    const { username } = useParams();
-    const [userProfile, setUserProfile] = useState({
-      bio: '',
-      profilePicture: '',
-      // Incluye otros campos que necesites
-    });
-    const [newBio, setNewBio] = useState('');
-    const [image, setImage] = useState('');
-    const history = useHistory();
-
-    useEffect(() => {
-      const fetchUserData = async () => {
-        try {
-          // Suponiendo que tienes una ruta de API para obtener datos de usuario por username
-          const response = await axios.get(`https://mern-stacksd-backend.onrender.com/api/users/${username}`);
-          setUserProfile(response.data);
-          setNewBio(response.data.bio);
-        } catch (error) {
-          console.error('Error fetching user data:', error);
-          // Manejar el error, por ejemplo redirigiendo a login si el usuario no está autenticado
-        }
-      };
-
-      fetchUserData();
-    }, [username]);
-
-    const handleBioChange = (e) => {
-      setNewBio(e.target.value);
-    };
-
-    const handleBioSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        // Suponiendo que tienes una ruta de API para actualizar la biografía
-        await axios.put(`https://mern-stacksd-backend.onrender.com/api/users/${username}/bio`, { bio: newBio });
-        setUserProfile(prevState => ({ ...prevState, bio: newBio }));
-      } catch (error) {
-        console.error('Error updating bio:', error);
-        // Manejar el error adecuadamente
-      }
-    };
-
-    const handleImageChange = (e) => {
-      setImage(e.target.files[0]);
-    };
-
-    const handleImageSubmit = async (e) => {
-      e.preventDefault();
-      const formData = new FormData();
-      formData.append('image', image);
-
-      try {
-        // Suponiendo que tienes una ruta de API para actualizar la imagen del perfil
-        const response = await axios.put(`https://mern-stacksd-backend.onrender.com/api/users/${username}/profile-picture`, formData, {
-          headers: { 'Content-Type': 'multipart/form-data' },
-        });
-        setUserProfile(prevState => ({ ...prevState, profilePicture: response.data.profilePicture }));
-      } catch (error) {
-        console.error('Error updating profile picture:', error);
-        // Manejar el error adecuadamente
-      }
-    };
-
-    return (
-      <div className="user-profile">
-        <form onSubmit={handleImageSubmit}>
-          <img src={userProfile.profilePicture || 'default-profile.png'} alt={`${username}'s profile`} />
-          <input type="file" onChange={handleImageChange} />
-          <button type="submit">Cambiar foto</button>
-        </form>
-        
-        <form onSubmit={handleBioSubmit}>
-          <textarea value={newBio} onChange={handleBioChange} />
-          <button type="submit">Cambiar biografía</button>
-        </form>
-      </div>
-    );
-};
-
-export default UserProfile;
+export default function UserProfile({ userInfo }) {
+  return (
+    <div className="user-profile">
+      <img src={userInfo.profilePicture} alt={`${userInfo.username}'s profile`} />
+      <h1>{userInfo.username}</h1>
+      <p>{userInfo.bio}</p>
+      {/* Formulario para cambiar la foto y la biografía si es necesario */}
+    </div>
+  );
+}
