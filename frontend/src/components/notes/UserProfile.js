4ac0d3fbbfbc259368e1ce5679f04f599859f93b
diegosva14 <diegosotovalle14@gmail.com>
2023-11-30 13:08:51 -0800

    commit

--- frontend/src/components/notes/UserProfile.js
+++ frontend/src/components/notes/UserProfile.js
@@ -3,36 +3,27 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const UserProfile = () => {
-  const { username } = useParams();
-  const [profile, setProfile] = useState({
-    bio: '',
-    profilePicture: '',
-  });
-
-  useEffect(() => {
-    // Obtén los datos del perfil del usuario
-    const fetchProfileData = async () => {
-      try {
-        const res = await axios.get(`https://mern-stacksd-backend.onrender.com/api/user/${username}`);
-        setProfile(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchProfileData();
-  }, [username]);
-
-  // ... manejo de la actualización del perfil ...
-
-  return (
-    <div>
-      {/* Formulario para mostrar y actualizar la foto de perfil y la biografía */}
-      <img src={profile.profilePicture} alt={`${username}'s profile`} />
-      <p>{profile.bio}</p>
-      {/* ... resto del formulario para actualizar ... */}
-    </div>
-  );
-};
-
-export default UserProfile;
+    const { username } = useParams();
+    const [userProfile, setUserProfile] = useState({
+      bio: '',
+      profilePicture: '',
+      // Incluye otros campos que necesites
+    });
+  
+    useEffect(() => {
+      // Aquí deberías realizar una petición para obtener los datos del usuario
+      // setUserProfile con la respuesta obtenida
+    }, [username]);
+  
+    // ... funciones para manejar la actualización de la foto y la biografía ...
+  
+    return (
+      <div className="user-profile">
+        <img src={userProfile.profilePicture} alt="Foto de perfil" />
+        <p>{userProfile.bio}</p>
+        {/* Botones o formularios para cambiar la foto y la biografía */}
+      </div>
+    );
+  };
+  
+  export default UserProfile;
