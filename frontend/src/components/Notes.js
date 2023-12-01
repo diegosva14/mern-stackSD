ed235399cce56b47624db0ab7f3b495ef6c63f38
diegosva14 <diegosotovalle14@gmail.com>
2023-11-30 16:14:15 -0800

    commit

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -3,7 +3,7 @@ import Header from './notes/Nav'
 import Home from './notes/Home'
 import CreateNote from './notes/CreateNote'
 import EditNote from './notes/EditNote'
-import UserProfile from './notes/UserProfile'; 
+
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 export default function Notes({setIsLogin,userInfo}) {
