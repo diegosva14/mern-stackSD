4ea761f758b7b1d566a80d0fca50e19017f54c32
diegosva14 <diegosotovalle14@gmail.com>
2023-12-01 00:37:08 -0800

    commit

--- backend/src/controllers/noteCtrl.js
+++ backend/src/controllers/noteCtrl.js
@@ -1,26 +1,22 @@
-// Importación de modelo de notas para interactuar con la base de datos.
-const Notes = require('../models/noteModel');
+const Notes = require('../models/noteModel')
 
 const noteCtrl = {
-    // Obtener todas las notas con posibilidad de ordenar por fecha o likes.
-    getNotes: async (req, res) => {
+
+      getNotes: async (req, res) => {
         try {
-            // Preparar criterios de ordenación basados en la consulta.
             let sortCriteria = {};
             if (req.query.sort === '-createdAt') {
                 sortCriteria = { createdAt: -1 };
             } else if (req.query.sort === '-likes') {
-                sortCriteria = { 'likes.length': -1 };
+                sortCriteria = { likes: -1 };
             }
-            // Obtener notas de la base de datos y devolverlas.
             const notes = await Notes.find().sort(sortCriteria);
             res.json(notes);
         } catch (err) {
             return res.status(500).json({ msg: err.message });
         }
     },
-
-    // Crear una nueva nota y asociarla con el usuario autenticado.
+      
     createNote: async(req, res) =>{
         try {
             const {title, content, date} = req.body;
@@ -28,29 +24,22 @@ const noteCtrl = {
                 title,
                 content,
                 date,
-                user_id: req.user.id, // ID del usuario obtenido del middleware de autenticación.
-                name: req.user.name // Nombre del usuario obtenido del middleware de autenticación.
-            });
-            await newNote.save();
-            res.json({msg: "Created a Note"});
+                user_id: req.user.id,
+                name: req.user.name
+            })
+            await newNote.save()
+            res.json({msg: "Created a Note"})
         } catch (err) {
-            return res.status(500).json({msg: err.message});
+            return res.status(500).json({msg: err.message})
         }
     },
 
-    // Eliminar una nota por ID. Solo el usuario que creó la nota puede eliminarla.
     deleteNote: async(req, res) =>{
         try {
-            // Verificar si el usuario autenticado es el autor de la nota.
-            const note = await Notes.findById(req.params.id);
-            if (note.user_id === req.user.id) {
-                await note.remove();
-                res.json({msg: "Deleted a Note"});
-            } else {
-                res.status(401).json({msg: "Unauthorized"});
-            }
+            await Notes.findByIdAndDelete(req.params.id)
+            res.json({msg: "Deleted a Note"})
         } catch (err) {
-            return res.status(500).json({msg: err.message});
+            return res.status(500).json({msg: err.message})
         }
     },
 
@@ -77,7 +66,6 @@ const noteCtrl = {
         }
       },
       
-      // Obtener una nota específica por su ID.
     getNote: async(req, res) => {
         try {
             const note = await Notes.findById(req.params.id)
@@ -86,7 +74,7 @@ const noteCtrl = {
             return res.status(500).json({msg: err.message})
         }
     },
-    // Manejar los 'likes' de una nota. Cualquier usuario autenticado puede dar o quitar un 'like'.
+
     likeNote: async (req, res) => {
         try {
           const note = await Notes.findById(req.params.id);
@@ -106,7 +94,7 @@ const noteCtrl = {
           return res.status(500).json({ msg: err.message });
         }
       },
-      // Añadir un comentario a una nota específica. Cualquier usuario autenticado puede comentar.
+      
       addComment: async (req, res) => {
         try {
           const note = await Notes.findById(req.params.id);
@@ -123,10 +111,6 @@ const noteCtrl = {
           return res.status(500).json({ msg: err.message });
         }
       },
-      
-      
-      
-      
       
 }
 
